70eb9049771a71d1f3af5336109de35547bc1dee
Benjamin Lemaire <benjamin.lemaire@franceinsoumise.org>
2025-04-22 16:33:56 +0200

    fix extract data from people command

--- agir/people/management/commands/extract_person.py
+++ agir/people/management/commands/extract_person.py
@@ -24,7 +24,14 @@ def field_expression(f):
     elif isinstance(f, CountryField):
         return (f.name, str)
     elif isinstance(f, StdImageField):
-        return (f.name, lambda u: settings.FRONT_DOMAIN + settings.MEDIA_URL + u.url)
+        return (
+            f.name,
+            lambda u: (
+                u.url
+                if u and getattr(u, "url", None)
+                else ""
+            ),
+        )
     elif f.name == "password":
         return Literal("(caché)")
 
