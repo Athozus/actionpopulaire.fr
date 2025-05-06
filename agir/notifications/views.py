1dc5612b3222931f51ddebde2ebdd322977345aa
Alexandra <bug.graph@gmail.com>
2025-05-06 11:13:46 +0200

    Revert "Fix N+1 query issue in subscription views by optimizing queryset preloading"
    This reverts commit 9fde2c9611aaacf69994100f883dfe3b742d1c4d.

--- agir/notifications/views.py
+++ agir/notifications/views.py
@@ -13,14 +13,13 @@ class ListCreateDestroySubscriptionAPIView(ListCreateAPIView, DestroyAPIView):
 
     def get_queryset(self):
         return self.queryset.filter(person=self.request.user.person).select_related(
-            "membership", "person"
+            "membership"
         )
 
     def get_object(self):
         if self.request.method == "DELETE":
-            subscription_ids = self.request.data.get("ids", [])
-            return self.get_queryset().filter(pk__in=subscription_ids)
-        return super().get_object()
+            return self.get_queryset().filter(pk__in=self.request.data)
+        return super(ListCreateDestroySubscriptionAPIView, self).get_object()
 
     def get_serializer(self, *args, **kwargs):
         kwargs.update({"many": True})
