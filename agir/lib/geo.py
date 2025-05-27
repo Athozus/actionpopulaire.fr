30ac13975fc5e0fee6c8fd7c83fdef5be5dc5973
Benjamin Lemaire <benjamin.lemaire@franceinsoumise.org>
2025-05-27 17:38:39 +0200

    increase commune cache timeout

--- agir/lib/geo.py
+++ agir/lib/geo.py
@@ -398,7 +398,7 @@ def build_commune_key(item):
     return hashlib.md5(raw_key.encode()).hexdigest()
 
 
-@redis_cache(timeout=600, key_func=lambda item: item.id, as_model_id=True)
+@redis_cache(timeout=None, key_func=lambda item: item.id, as_model_id=True)
 def get_commune(item) -> Commune:
     commune = None
 
