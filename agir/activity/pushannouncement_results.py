1d94a2eb1fefa98902460f5bd31cebf7740c6b7c
Alexandra <bug.graph@gmail.com>
2025-06-26 14:30:30 +0200

    chore: upgrade firebase

--- agir/activity/pushannouncement_results.py
+++ agir/activity/pushannouncement_results.py
@@ -1,5 +1,6 @@
 from agir.api.redis import get_auth_redis_client as get_redis_client
 
+
 def get_as_int_or_default(value, default):
     if value is None:
         return default
@@ -17,7 +18,9 @@ class PushAnnouncementResults:
         return self.get_redis_key() + "_failures"
 
     def get_failures_amount(self):
-        return get_as_int_or_default(get_redis_client().get(self.get_redis_failure_key()), 0)
+        return get_as_int_or_default(
+            get_redis_client().get(self.get_redis_failure_key()), 0
+        )
 
     def incr_failures_amount(self, amount: int = 1):
         return get_redis_client().incr(self.get_redis_failure_key(), amount)
@@ -26,7 +29,9 @@ class PushAnnouncementResults:
         return self.get_redis_key() + "_success"
 
     def get_success_amount(self):
-        return get_as_int_or_default(get_redis_client().get(self.get_redis_success_key()), 0)
+        return get_as_int_or_default(
+            get_redis_client().get(self.get_redis_success_key()), 0
+        )
 
     def incr_success_amount(self, amount: int = 1):
         return get_redis_client().incr(self.get_redis_success_key(), amount)
